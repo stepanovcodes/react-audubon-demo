2ae22f50853623e009bc73566ee087f2f0a361af
Alex Stepanov <stepanov.hw@gmail.com>
2023-08-25 13:36:07 -0600

    Do step 8

--- src/Pages/Birds/Birds.jsx
+++ src/Pages/Birds/Birds.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Birds = (props) => {
   const [birds, setBirds] = useState([]); // <--Set the initial state here by passing it an empty array []
@@ -9,7 +10,7 @@ const Birds = (props) => {
         "https://ga-audubon-api.herokuapp.com/api/birds"
       );
       const birdData = await response.json();
-    //   console.log(birdData);
+      //   console.log(birdData);
       setBirds(birdData);
     } catch (err) {
       console.log(err);
@@ -27,17 +28,16 @@ const Birds = (props) => {
     <section className="container">
       {birds.map((bird) => {
         return (
-          <div className="card" key={bird._id}>
-            <div className="card-image">
-              <img
-                src={bird.image}
-                alt={bird.name}
-              />
+          <Link to={`/details/${bird._id}`} key={bird._id}>
+            <div className="card">
+              <div className="card-image">
+                <img src={bird.image} alt={bird.name} />
+              </div>
+              <div className="card-title">
+                <h3>{bird.name}</h3>
+              </div>
             </div>
-            <div className="card-title">
-              <h3>{bird.name}</h3>
-            </div>
-          </div>
+          </Link>
         );
       })}
     </section>
